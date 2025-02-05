008272d51667cad78ecc5712888e094b9d0fa978
jasmirmedina <jasmirmedina56@gmail.com>
2025-02-05 05:13:40 -0600

    chore: rename

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import "./base.css";
 
 import handleDates from "./scripts/handle-dates";
-import dialogAnimations from "./scripts/dialog-animations";
+import handleDialog from "./scripts/handle-dialog";
 import handleSidebar from "./scripts/handle-sidebar";
 
 handleDates();
-dialogAnimations();
+handleDialog();
 handleSidebar();
