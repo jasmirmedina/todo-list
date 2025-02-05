08edc45b04992937098045d7ad4f500d39373937
jasmirmedina <jasmirmedina56@gmail.com>
2025-02-05 16:30:09 -0600

    feat: add tasks dialog

--- src/scripts/handle-dialog.js
+++ src/scripts/handle-dialog.js
@@ -12,7 +12,6 @@ const addDialogAnimations = (dialog, className, ...args) => {
 
 const handleDialog = () => {
   const projectsDialog = $(".projects-dialog");
-
   const sidebarAddProjectBtn = $(".sidebar__add-project-btn");
   const cancelProjectBtn = $(".projects-dialog__cancel-project-btn");
 
@@ -21,6 +20,17 @@ const handleDialog = () => {
     "projects-dialog--is-hidden",
     sidebarAddProjectBtn,
     cancelProjectBtn,
+  );
+
+  const tasksDialog = $(".tasks-dialog");
+  const mainAddTaskBtn = $(".main__add-task-btn");
+  const cancelTaskBtn = $(".tasks-dialog__cancel-task-btn");
+
+  addDialogAnimations(
+    tasksDialog,
+    "tasks-dialog--is-hidden",
+    mainAddTaskBtn,
+    cancelTaskBtn,
   );
 };
 
