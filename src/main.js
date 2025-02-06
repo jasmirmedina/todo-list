7cbea992c863bb9f35e357473d38755163fcc4e4
jasmirmedina <jasmirmedina56@gmail.com>
2025-02-05 18:22:37 -0600

    chore: change initial logic

--- src/main.js
+++ src/main.js
@@ -1,14 +1,11 @@
 import "./base.css";
 
-import { checkLocalStorage } from "./scripts/utils";
-
 import initial from "./scripts/initial";
 import handleDates from "./scripts/handle-dates";
 import handleDialog from "./scripts/handle-dialog";
 import handleSidebar from "./scripts/handle-sidebar";
 
-if (checkLocalStorage("projects")) initial();
-
+initial();
 handleDates();
 handleDialog();
 handleSidebar();
--- src/scripts/initial.js
+++ src/scripts/initial.js
@@ -3,15 +3,17 @@ import projects from "./projects";
 import Project from "./Project";
 import Task from "./Task";
 
+import { checkLocalStorage } from "./utils";
+
 const initial = () => {
-  const defaultProject = new Project("Default");
-  const defaultTask = new Task("Default", "This is a description", 0);
+  if (checkLocalStorage("projects")) {
+    const defaultProject = new Project("Default");
+    const defaultTask = new Task("Default", "This is a description", 0);
 
-  defaultProject.tasks.push(defaultTask);
+    defaultProject.tasks.push(defaultTask);
 
-  projects.push(defaultProject);
-
-  console.log(projects);
+    projects.push(defaultProject);
+  }
 };
 
 export default initial;
