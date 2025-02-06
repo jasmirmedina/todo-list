bc46ede6da04134e189f44bba94a1908f09303c5
jasmirmedina <jasmirmedina56@gmail.com>
2025-02-05 18:24:22 -0600

    chore: remove unused property

--- src/scripts/initial.js
+++ src/scripts/initial.js
@@ -8,7 +8,7 @@ import { checkLocalStorage } from "./utils";
 const initial = () => {
   if (checkLocalStorage("projects")) {
     const defaultProject = new Project("Default");
-    const defaultTask = new Task("Default", "This is a description", 0);
+    const defaultTask = new Task("Default", 0);
 
     defaultProject.tasks.push(defaultTask);
 
